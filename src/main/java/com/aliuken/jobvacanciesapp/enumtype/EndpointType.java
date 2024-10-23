b33d5cf8bd921c94f363a29c0b4808b110bfd460
Aliuken <alber84ou@gmail.com>
2024-10-23 16:49:44 +0200

    a

--- src/main/java/com/aliuken/jobvacanciesapp/enumtype/EndpointType.java
+++ src/main/java/com/aliuken/jobvacanciesapp/enumtype/EndpointType.java
@@ -109,7 +109,7 @@ public enum EndpointType implements Serializable {
 
 	private static final Map<EndpointRegexPatternDTO, EndpointType> ENDPOINT_TYPE_MAP = EndpointType.getEndpointTypeMap();
 
-	private EndpointType(final HttpMethod  httpMethod, final String pathRegex, final String description) {
+	private EndpointType(final HttpMethod httpMethod, final String pathRegex, final String description) {
 		this.endpointRegexPatternDTO = EndpointRegexPatternDTO.getNewInstance(httpMethod, pathRegex, description);
 	}
 
@@ -125,7 +125,7 @@ public enum EndpointType implements Serializable {
 		for(final Map.Entry<EndpointRegexPatternDTO, EndpointType> endpointTypeMapEntry : ENDPOINT_TYPE_MAP.entrySet()) {
 			final EndpointRegexPatternDTO endpointRegexPatternDTO = endpointTypeMapEntry.getKey();
 
-			boolean matches = endpointRegexPatternDTO.matches(httpMethod, path);
+			final boolean matches = endpointRegexPatternDTO.matches(httpMethod, path);
 
 			if(matches) {
 				final EndpointType endpointType = endpointTypeMapEntry.getValue();
