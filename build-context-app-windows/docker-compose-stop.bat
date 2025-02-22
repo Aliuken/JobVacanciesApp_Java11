b5c363940c60fd288609a3eca4e3f668e3f266fc
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-02-22 22:07:06 +0100

    a

---
+++ build-context-app-windows/docker-compose-stop.bat
@@ -0,0 +1,6 @@
+cd C:\Programacion\git\JobVacanciesApp_Java11\build-context-app-windows
+
+docker compose stop
+docker compose down
+
+pause
--- build-context-app/docker-compose-stop.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Programacion\git\JobVacanciesApp_Java11\build-context-app
-
-docker compose stop
-docker compose down
-
-pause
