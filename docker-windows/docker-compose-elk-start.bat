44d534adbb5927f2c61d3122e6f7e391c0c79f94
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-02-23 21:12:29 +0100

    a

--- docker-windows/docker-compose-elk-start.bat
+++ docker-windows/docker-compose-elk-start.bat
@@ -1,3 +1,3 @@
 clear
 
-runas /user:Usuario "C:\Programacion\git\JobVacanciesApp_Java11\build-context-elk\docker-compose-start.bat"
+runas /user:Usuario "C:\Programacion\git\JobVacanciesApp_Java11\build-context-elk-windows\docker-compose-start.bat"
