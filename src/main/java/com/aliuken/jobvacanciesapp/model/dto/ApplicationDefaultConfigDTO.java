544a731a485c5114a8ce624b3852be786330b1ba
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-12-05 10:04:06 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/model/dto/ApplicationDefaultConfigDTO.java
+++ src/main/java/com/aliuken/jobvacanciesapp/model/dto/ApplicationDefaultConfigDTO.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class ApplicationDefaultConfigDTO implements Serializable {
 	private static final long serialVersionUID = 7326190421458078482L;
 
-    private static final ApplicationDefaultConfigDTO NO_ARGS_INSTANCE = new ApplicationDefaultConfigDTO(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+	private static final ApplicationDefaultConfigDTO NO_ARGS_INSTANCE = new ApplicationDefaultConfigDTO(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
 	//Non-overwritable properties
 	private final String authUserCurriculumFilesPath;
 	private final String authUserEntityQueryFilesPath;
@@ -31,7 +31,7 @@ public class ApplicationDefaultConfigDTO implements Serializable {
 	//Overwritable properties
 	private final Language defaultLanguage;
 	private final AnonymousAccessPermission defaultAnonymousAccessPermission;
-    private final TableSortingDirection defaultInitialTableSortingDirection;
+	private final TableSortingDirection defaultInitialTableSortingDirection;
 	private final TablePageSize defaultInitialTablePageSize;
 	private final ColorMode defaultColorMode;
 	private final UserInterfaceFramework defaultUserInterfaceFramework;
@@ -50,28 +50,28 @@ public class ApplicationDefaultConfigDTO implements Serializable {
 		final String resetPasswordLinkExpirationHoursString = Objects.toString(resetPasswordLinkExpirationHours);
 		final String defaultLanguageName = Objects.toString(defaultLanguage);
 		final String defaultAnonymousAccessPermissionName = Objects.toString(defaultAnonymousAccessPermission);
-        final String defaultInitialTableSortingDirectionName = Objects.toString(defaultInitialTableSortingDirection);
+		final String defaultInitialTableSortingDirectionName = Objects.toString(defaultInitialTableSortingDirection);
 		final String defaultInitialTablePageSizeName = Objects.toString(defaultInitialTablePageSize);
 		final String defaultColorModeName = Objects.toString(defaultColorMode);
 		final String defaultUserInterfaceFrameworkName = Objects.toString(defaultUserInterfaceFramework);
 		final String defaultPdfDocumentPageFormatName = Objects.toString(defaultPdfDocumentPageFormat);
 
 		final String result = StringUtils.getStringJoined(
-				"ApplicationDefaultConfigDTO [authUserCurriculumFilesPath=", authUserCurriculumFilesPath,
-				", authUserEntityQueryFilesPath=", authUserEntityQueryFilesPath,
-				", jobCompanyLogosPath=", jobCompanyLogosPath,
-				", useAjaxToRefreshJobCompanyLogos=", useAjaxToRefreshJobCompanyLogosString,
-				", useEntityManagerCache=", useEntityManagerCacheString,
-				", useParallelStreams=", useParallelStreamsString,
-				", signupConfirmationLinkExpirationHours=", signupConfirmationLinkExpirationHoursString,
-				", resetPasswordLinkExpirationHours=", resetPasswordLinkExpirationHoursString,
-				", defaultLanguage=", defaultLanguageName,
-				", defaultAnonymousAccessPermission=", defaultAnonymousAccessPermissionName,
-                ", defaultInitialTableSortingDirection=", defaultInitialTableSortingDirectionName,
-				", defaultInitialTablePageSize=", defaultInitialTablePageSizeName,
-				", defaultColorMode=", defaultColorModeName,
-				", defaultUserInterfaceFramework=", defaultUserInterfaceFrameworkName,
-				", defaultPdfDocumentPageFormat=", defaultPdfDocumentPageFormatName, "]");
+			"ApplicationDefaultConfigDTO [authUserCurriculumFilesPath=", authUserCurriculumFilesPath,
+			", authUserEntityQueryFilesPath=", authUserEntityQueryFilesPath,
+			", jobCompanyLogosPath=", jobCompanyLogosPath,
+			", useAjaxToRefreshJobCompanyLogos=", useAjaxToRefreshJobCompanyLogosString,
+			", useEntityManagerCache=", useEntityManagerCacheString,
+			", useParallelStreams=", useParallelStreamsString,
+			", signupConfirmationLinkExpirationHours=", signupConfirmationLinkExpirationHoursString,
+			", resetPasswordLinkExpirationHours=", resetPasswordLinkExpirationHoursString,
+			", defaultLanguage=", defaultLanguageName,
+			", defaultAnonymousAccessPermission=", defaultAnonymousAccessPermissionName,
+			", defaultInitialTableSortingDirection=", defaultInitialTableSortingDirectionName,
+			", defaultInitialTablePageSize=", defaultInitialTablePageSizeName,
+			", defaultColorMode=", defaultColorModeName,
+			", defaultUserInterfaceFramework=", defaultUserInterfaceFrameworkName,
+			", defaultPdfDocumentPageFormat=", defaultPdfDocumentPageFormatName, "]");
 		return result;
 	}
 }
