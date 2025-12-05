14b82b9e07a495d728a357a4185fc771426aba7a
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-12-05 10:01:49 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/model/dto/BigDecimalFromStringConversionResult.java
+++ src/main/java/com/aliuken/jobvacanciesapp/model/dto/BigDecimalFromStringConversionResult.java
@@ -11,31 +11,31 @@ import java.util.function.Function;
 
 @Data
 public class BigDecimalFromStringConversionResult implements Serializable {
-    private static final long serialVersionUID = 417055475888522041L;
+	private static final long serialVersionUID = 417055475888522041L;
 
-    private static final BigDecimalFromStringConversionResult NO_ARGS_INSTANCE = new BigDecimalFromStringConversionResult(null, null);
+	private static final BigDecimalFromStringConversionResult NO_ARGS_INSTANCE = new BigDecimalFromStringConversionResult(null, null);
 
-    private final Function<Language, String> conversionErrorFunction;
-    private final BigDecimal conversionResult;
+	private final Function<Language, String> conversionErrorFunction;
+	private final BigDecimal conversionResult;
 
-    public static BigDecimalFromStringConversionResult getNewInstance() {
-        return NO_ARGS_INSTANCE;
-    }
+	public static BigDecimalFromStringConversionResult getNewInstance() {
+		return NO_ARGS_INSTANCE;
+	}
 
-    public static BigDecimalFromStringConversionResult getNewInstanceWithError(Function<Language, String> conversionErrorFunction) {
-        return new BigDecimalFromStringConversionResult(conversionErrorFunction, null);
-    }
+	public static BigDecimalFromStringConversionResult getNewInstanceWithError(Function<Language, String> conversionErrorFunction) {
+		return new BigDecimalFromStringConversionResult(conversionErrorFunction, null);
+	}
 
-    public static BigDecimalFromStringConversionResult getNewInstanceWithoutError(BigDecimal conversionResult) {
-        return new BigDecimalFromStringConversionResult(null, conversionResult);
-    }
+	public static BigDecimalFromStringConversionResult getNewInstanceWithoutError(BigDecimal conversionResult) {
+		return new BigDecimalFromStringConversionResult(null, conversionResult);
+	}
 
-    @Override
-    public String toString() {
-        final String conversionErrorFunctionString = Objects.toString(conversionErrorFunction);
-        final String conversionResultString = Objects.toString(conversionResult);
+	@Override
+	public String toString() {
+		final String conversionErrorFunctionString = Objects.toString(conversionErrorFunction);
+		final String conversionResultString = Objects.toString(conversionResult);
 
-        final String result = StringUtils.getStringJoined("BigDecimalFromStringConversionResult [conversionErrorFunction=", conversionErrorFunctionString, ", conversionResult=", conversionResultString, "]");
-        return result;
-    }
+		final String result = StringUtils.getStringJoined("BigDecimalFromStringConversionResult [conversionErrorFunction=", conversionErrorFunctionString, ", conversionResult=", conversionResultString, "]");
+		return result;
+	}
 }
